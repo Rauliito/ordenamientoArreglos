c4dc76bd87a993cb416bf9c4bf5f9384c2caa073
USUARIO <USUARIO@PF17>
2018-04-24 19:56:09 -0500

    Se crea la clase Estudiante

--- src/ordenamientoobjetos/OrdenamientoObjetos.java
+++ src/ordenamientoobjetos/OrdenamientoObjetos.java
@@ -6,6 +6,13 @@ public class OrdenamientoObjetos {
     public static void main (String[] args) {
         Estudiante [] arrayEstudiantes = new Estudiante [5];
         arrayEstudiantes[0] = new Estudiante ("raul", "farfan","123", 5, 5.0);
+        arrayEstudiantes[0] = new Estudiante ("delman", "suarez","123", 4, 4.5);
+        arrayEstudiantes[0] = new Estudiante ("sebastian", "hernandez","123", 3, 4.8);
+        arrayEstudiantes[0] = new Estudiante ("alberto", "bolano","123", 2, 4.2);
+        arrayEstudiantes[0] = new Estudiante ("nicolas", "ramos","123", 1, 2.0);
+        arrayEstudiantes[0] = new Estudiante ("juan", "jaime","123", 6, 3.5);
+        
+        
         
         
     }
